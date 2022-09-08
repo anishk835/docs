1be2fdd0ee090a1f7cff5d282c539ac96607808c
Anish <anishk835@gmail.com>
2022-09-08 14:44:26 +0200

    Update install-node-using-npm.sh

--- node/install-node-using-npm.sh
+++ node/install-node-using-npm.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+function install_node_stable() {
+  npm cache clean -f
+  npm install -g n
+  sudo n stable
+}
+
+function install_node_latest() {
+  npm cache clean -f
+  npm install -g n
+  sudo n latest
+}
+
+function install_node_specific_version() {
+  npm cache clean -f
+  npm install -g n
+  sudo n $1
+}
